0e0a846e8d132427f2cc2371bd49d25ef01398d5
Staci Rosales <stacileep2@gmail.com>
2023-05-20 21:48:00 -0500

    Update controllers

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,20 +1,20 @@
 class ClientController < Sinatra::Base
     set :default_content_type, 'application/json'
 
-  get "/client" do 
-    clients = Client.all
-    clients.to_json
-  end
+    get "/client" do 
+        clients = Client.all
+        clients.to_json
+    end
 
-  post '/client' do
-    new_client = Client.create(params)
-    new_client.to_json
-  end
+    post '/client' do
+        new_client = Client.create(params)
+        new_client.to_json
+    end
 
-  delete '/client/:id' do
-    client = Client.find(params[:id])
-    client.destroy
-    client.to_json
-  end
-  
+    delete '/client/:id' do
+        client = Client.find(params[:id])
+        client.destroy
+        client.to_json
+    end
+
 end
--- app/controllers/therapist_controller.rb
+++ app/controllers/therapist_controller.rb
@@ -1,8 +1,9 @@
 class TherapistController < Sinatra::Base
-
+    set :default_content_type, 'application/json'
+    
     get "/therapist" do
         therapists = Therapist.all
         therapists.to_json
     end
-    
+
 end
