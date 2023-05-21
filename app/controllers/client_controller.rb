49fd507a628d7a5145ee7755c816076a9f66f62f
Staci Rosales <stacileep2@gmail.com>
2023-05-20 21:51:19 -0500

    update controllers with CRUD

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -11,6 +11,12 @@ class ClientController < Sinatra::Base
         new_client.to_json
     end
 
+    patch '/client/:id' do
+        patch_client = Client.find(params[:id])
+        patch_client.update(params)
+        patch_client.to_json
+    end
+
     delete '/client/:id' do
         client = Client.find(params[:id])
         client.destroy
